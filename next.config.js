421cd4e4479ebf3c9bbdd68b30d6e0138638e585
NisuSchnisuu <242468587+NisuSchnisuu@users.noreply.github.com>
2026-01-26 23:51:13 +0100

    Error Fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/ImageGeneration_6b' : '',
     // Erlaubt Bilder von externen Quellen (wichtig für die generierten Bilder)
     images: {
         unoptimized: true,
