777adb483a18713219cf006529cfe9a62c2b80b2
NisuSchnisuu <242468587+NisuSchnisuu@users.noreply.github.com>
2026-01-29 17:16:28 +0100

    function deploy

--- supabase/functions/bild-generieren/index.ts
+++ supabase/functions/bild-generieren/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { GoogleGenerativeAI } from "https://esm.sh/@google/generative-ai";
+import { GoogleGenerativeAI } from "https://esm.sh/@google/generative-ai@0.12.0";
 
 const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
